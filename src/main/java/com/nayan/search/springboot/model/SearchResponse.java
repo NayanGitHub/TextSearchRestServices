2e3758afec04cdce7c03c211db3b75541690e889
Nayan Barman <nayanbarman@Nayans-MacBook-Pro.local>
2020-06-10 15:18:31 +1000

    Slight modification

--- src/main/java/com/nayan/search/springboot/model/SearchResponse.java
+++ src/main/java/com/nayan/search/springboot/model/SearchResponse.java
@@ -2,17 +2,13 @@ package com.nayan.search.springboot.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
-import org.json.JSONObject;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 @JsonAutoDetect
-public class SearchResponse implements Serializable{
-	
+public class SearchResponse implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Map<String, String>> counts;
 
@@ -24,6 +20,7 @@ public class SearchResponse implements Serializable{
 	public SearchResponse() {
 
 	}
+
 	@JsonProperty
 	public ArrayList<Map<String, String>> getCounts() {
 		return counts;
